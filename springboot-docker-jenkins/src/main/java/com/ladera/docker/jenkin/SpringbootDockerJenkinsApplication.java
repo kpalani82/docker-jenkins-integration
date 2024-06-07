e25ae5b8dd77794aece7ebf7376f00bb6e9da219
Palani Kuppusami <palani.kuppusami@sap.com>
2024-06-07 20:33:15 +0530

    test

--- springboot-docker-jenkins/src/main/java/com/ladera/docker/jenkin/SpringbootDockerJenkinsApplication.java
+++ springboot-docker-jenkins/src/main/java/com/ladera/docker/jenkin/SpringbootDockerJenkinsApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDockerJenkinsApplication {
 
+
 	public static void main(String[] args) {
+
+		System.out.println("hi");
 		SpringApplication.run(SpringbootDockerJenkinsApplication.class, args);
 	}
 
