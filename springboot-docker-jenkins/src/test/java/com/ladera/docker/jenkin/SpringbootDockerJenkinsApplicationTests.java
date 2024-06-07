0cd7f407ff3d8a339b81bc7bb6dfb38e27018514
Palani Kuppusami <palani.kuppusami@sap.com>
2024-06-07 17:12:20 +0530

    first commit

---
+++ springboot-docker-jenkins/src/test/java/com/ladera/docker/jenkin/SpringbootDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladera.docker.jenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
